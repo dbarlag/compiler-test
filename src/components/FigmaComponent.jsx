bfebe8b02c14cc259e69ba5140c6d8522c633912
danielbarlag <dan.barlag@gmail.com>
2026-03-11 08:41:13 +0100

    Update from Figma MCP Compiler

--- src/components/FigmaComponent.jsx
+++ src/components/FigmaComponent.jsx
@@ -22,9 +22,9 @@ export default function FigmaComponent() {
           <Donor amounts={[{"value":220,"label":"220 kr"},{"value":345,"label":"345 kr"},{"value":660,"label":"660 kr"}]} defaultAmount={345} oneTimeLabel="En gang" monthlyLabel="Hver måned" customAmountPlaceholder="Valgfritt beløp" amountLabel="Velg ønsket beløp:" impactMessage="En gave på {amount} bidrar til lorem ipsum dolor sit amet" vippsButtonLabel="Gi med" avtalegiroLabel="Gi med Kort" avtalegiroHref="#" currencySuffix="kr" data-color="primary" showVippsButton showAvtalegiroLink showImpactMessage />
         </div>
         <section data-figma-id="657:22053" data-figma-name="Girl section" className={styles.GirlSection6}>
-          <img data-figma-id="646:22322" data-figma-name="Redcross-graphics-1" src="https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/44402074-4369-462c-b50b-e2f924b81e4a" alt="Redcross-graphics-1" className={styles.Redcrossgraphics13} />
-          <img data-figma-id="647:22901" data-figma-name="girl" src="https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/68fcf523-44d1-4e05-8def-04eb08cdd68d" alt="girl" className={styles.girl4} />
-          <img data-figma-id="646:22327" data-figma-name="Redcross-graphics-1" src="https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/e3ce4369-7e71-412f-b81f-741562b237a0" alt="Redcross-graphics-1" className={styles.Redcrossgraphics15} />
+          <img data-figma-id="646:22322" data-figma-name="Redcross-graphics-1" src="https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/c630dd4c-c255-4cdf-8912-9feb659f9cfc" alt="Redcross-graphics-1" className={styles.Redcrossgraphics13} />
+          <img data-figma-id="647:22901" data-figma-name="girl" src="https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/10264399-0fdd-461f-8bd3-1c7295893cf5" alt="girl" className={styles.girl4} />
+          <img data-figma-id="646:22327" data-figma-name="Redcross-graphics-1" src="https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/a15cc327-6284-41fd-990d-5b65b6b10001" alt="Redcross-graphics-1" className={styles.Redcrossgraphics15} />
         </section>
       </section>
       <section data-figma-id="316:21652" data-figma-name="Section2: Call to action" className={styles.Section2CallToAction10}>
@@ -79,9 +79,9 @@ export default function FigmaComponent() {
         </div>
         <div data-figma-id="316:21708" data-figma-name="texts" className={styles.texts28}>
           <h4 data-figma-id="316:21710" data-figma-name="Heading" className={styles.Heading25}>Vi avdekker og hindrer nød</h4>
-          <img data-figma-id="316:21724" data-figma-name="Line 1" src="https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/0377b03a-1d44-4311-aa4b-9a34cf7e6937" alt="Line" className={styles.Line126} />
+          <img data-figma-id="316:21724" data-figma-name="Line 1" src="https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/68dca1e0-d387-4d2a-b6c6-77b37cd8604b" alt="Line" className={styles.Line126} />
           <h4 data-figma-id="316:21726" data-figma-name="Heading" className={styles.Heading27}>Vi gir rask respons og lindrer når krisen oppstår</h4>
-          <img data-figma-id="316:21740" data-figma-name="Line 2" src="https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/7fb9d8e6-95c5-4a2a-a935-5746777d9135" alt="Line" className={styles.Line126} />
+          <img data-figma-id="316:21740" data-figma-name="Line 2" src="https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/8fdb60c6-6bfa-4e2a-a4c1-e1b5fc1a2b6f" alt="Line" className={styles.Line126} />
           <h4 data-figma-id="316:21742" data-figma-name="Heading" className={styles.Heading25}>Vi avdekker og hindrer nød</h4>
         </div>
       </section>
@@ -96,12 +96,12 @@ export default function FigmaComponent() {
                 <Paragraph data-size="md" variant="long" data-figma-id="316:21761" data-figma-name="Body">Call out a feature, benefit, or value of your site, then link to a page where people can learn more about it.</Paragraph>
               </div>
             </div>
-            <Button variant="primary" data-color="primary" data-figma-id="316:21762" data-figma-name="Button">A button</Button>
+            <Button variant="primary" data-color="primary" data-figma-id="316:21762" data-figma-name="Button" onClick={showHelloWorld}>A button</Button>
           </div>
-          <img data-figma-id="316:21763" data-figma-name="Image" src="https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/d0d4b550-99ac-436a-8a87-870e0c64643d" alt="" className={styles.Image32} />
+          <img data-figma-id="316:21763" data-figma-name="Image" src="https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/16b599db-6b23-4dde-865b-940dd0f1fe8d" alt="" className={styles.Image32} />
         </div>
         <div data-figma-id="316:21764" data-figma-name="Row 2" className={styles.Row235}>
-          <img data-figma-id="316:21765" data-figma-name="Image" src="https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/cc9eb9e0-76b3-4ada-aa57-f4fd8a0e7035" alt="" className={styles.Image34} />
+          <img data-figma-id="316:21765" data-figma-name="Image" src="https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/5be7fa6d-4cc8-420d-a1fb-53fe53b4de4a" alt="" className={styles.Image34} />
           <div data-figma-id="316:21766" data-figma-name="Content" className={styles.Content31}>
             <div data-figma-id="316:21767" data-figma-name="Text" className={styles.Text30}>
               <div className={styles.dsWrapper12}>
